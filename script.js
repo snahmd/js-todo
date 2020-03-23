3223c06d5bb7b29a64478fb56812eb89eade2184
snahmd <sanahmed7@gmail.com>
2020-03-23 23:44:46 +0100

    delete element

--- script.js
+++ script.js
@@ -5,12 +5,21 @@ const input = document.querySelector('#txtTaskName');
 const btnDeleteAll = document.querySelector('#btnDeleteAll');
 const taskList = document.querySelector('#task-list');
 
+//call event listeners
 eventListeners();
 
 function eventListeners(){
+  // submit event
   form.addEventListener('submit', addNewItem);
+
+  // delete an item
+  taskList.addEventListener('click', deleteItem);
+
+  // delete all items
+  btnDeleteAll.addEventListener('click', deleteAllItems);
 }
 
+//add new item
 function addNewItem(e){
 
   if(input.value ===''){
@@ -35,6 +44,23 @@ function addNewItem(e){
   taskList.appendChild(li);
 
   input.value='';
+
+  e.preventDefault();
+}
+
+//delete an item
+function deleteItem(e){
+  if(e.target.className === 'fas fa-times'){
+    e.target.parentElement.parentElement.remove();
+  }
+
+  e.preventDefault();
+}
+
+// delete all items
+function deleteAllItems(){
+
+  taskList.innerHTML = '';
 
   e.preventDefault();
 }
